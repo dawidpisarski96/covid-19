ec6a9148c84209554da7add215cf8bfcdb7bfdd6
dawid <dawidpisarski96@gmail.com>
2020-04-17 17:42:10 +0200

    Updates

---
+++ precache-manifest.86600140d1b9bf10cdf0ebdc50653703.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e95976b8f52dac70342ffe796d33c3c",
+    "url": "/covid-19/index.html"
+  },
+  {
+    "revision": "5ec14651236b2f8e3a45",
+    "url": "/covid-19/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "82a1ffa5b38268268be4",
+    "url": "/covid-19/static/js/2.9242fcc9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/covid-19/static/js/2.9242fcc9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ec14651236b2f8e3a45",
+    "url": "/covid-19/static/js/main.790e423c.chunk.js"
+  },
+  {
+    "revision": "5947671d1c53a9002efe",
+    "url": "/covid-19/static/js/runtime-main.db8c32d2.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/covid-19/static/media/logo.5d5d9eef.svg"
+  }
+]);
